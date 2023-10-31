c8f9a71f1553416c904019c6cf6ac6925d6afea8
Velocifero1052 <commelevent1052@yandex.ru>
2023-10-31 09:23:20 +0500

    small refactoring

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 import {containsCommonItem3} from "./common_items_of_array.js";
 import {hasPairWithSum2} from './has_pair_with_sum_google.js';
 
-
 const array1 = ['a', 'b', 'c', 'x'];
 const array2 = ['z', 'a', 'y'];
-
 
 const arr = [1, 2, 3, 4, 5, 6];
 const sum = 5;
@@ -12,3 +10,5 @@ const sum = 5;
 console.log(hasPairWithSum2(arr, sum));
 
 console.log(containsCommonItem3(array1, array2));
+
+

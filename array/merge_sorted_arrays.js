cfbc6c5284ca567ac3fa17f1e204f39e73e99140
Velocifero1052 <commelevent1052@yandex.ru>
2023-10-31 10:30:52 +0500

    comments added

--- array/merge_sorted_arrays.js
+++ array/merge_sorted_arrays.js
@@ -68,7 +68,6 @@ function mergeSortedArray(array1, array2) {
   return mergedArray;
 }
 
-
 let firstSortedArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
 let secondSortedArray = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 
